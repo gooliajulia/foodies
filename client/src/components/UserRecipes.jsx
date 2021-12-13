c87f9bab292ec2d1977eeb44ad102a984c9e16b9
gooliajulia929 <juliadwyer93@gmail.com>
2021-12-13 12:15:23 -0500

    user recipes and basic css

--- client/src/components/UserRecipes.jsx
+++ client/src/components/UserRecipes.jsx
@@ -1,3 +1,4 @@
+import './UserRecipes.css';
 
 import { Link } from 'react-router-dom';
 
@@ -6,11 +7,19 @@ export default function UserRecipes({recipes}) {
     return (
         <div>
             <h2>My Recipes</h2>
-            <h3>{recipes[0].name}</h3>
+            <div className='column-titles'>
+                <h4>Name</h4>
+                <h4>Ingredients</h4>
+                <h4>Prep Time</h4>
+            </div>
             {recipes?.map((recipe) => (
                 <div key={recipe.id}>
                     <Link to={`/home/recipes/${recipe.id}`}>
-                        <p>{recipe.name}: {recipe.ingredients}</p>
+                        <div className='recipe-link'>
+                            <p className='first'>{recipe.name}</p>
+                            <p className='center'>{recipe.ingredients}</p>
+                            <p>prep time</p>
+                        </div>
                     </Link>
                 </div>
             ))}
