d47a457e44f0bda1c510c58e5981c49778e0580b
gooliajulia929 <juliadwyer93@gmail.com>
2021-12-14 14:10:35 -0500

    remove floating string in apiConfig

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-'https://foodies-app-api.herokuapp.com/'
-
 const baseUrl = process.env.NODE_ENV === 'production' 
     ?  'https://foodies-app-api.herokuapp.com/'
     : 'http://localhost:3000' 
