32641b6c57bf444bb90ee69bcd5856c42e941948
gooliajulia929 <juliadwyer93@gmail.com>
2021-12-14 14:07:25 -0500

    add CI to package.json

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3000';
+'https://foodies-app-api.herokuapp.com/'
+
+const baseUrl = process.env.NODE_ENV === 'production' 
+    ?  'https://foodies-app-api.herokuapp.com/'
+    : 'http://localhost:3000' 
 
 const api = axios.create({
     baseURL: baseUrl
