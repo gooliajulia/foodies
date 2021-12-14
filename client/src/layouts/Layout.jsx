29b056339ac2ced94e1813ba04353cc01d5c62ac
gooliajulia929 <juliadwyer93@gmail.com>
2021-12-13 20:59:15 -0500

    left nav panel css major improvements and add icons

--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -10,38 +10,40 @@ export default function Layout({currentUser, users, children}) {
                     <nav>
 
                         <Link className='nav' to='/home'>
-                            <img className='icon' src="https://img.icons8.com/fluency-systems-regular/48/000000/home.png"/><p>Home</p>
+                            <img className='icon' src="https://img.icons8.com/fluency-systems-regular/48/000000/home.png" alt='house'/><p>Home</p>
                         </Link>
                         <Link className='nav' to='/search'>
-                        <img className='icon' src="https://img.icons8.com/ios/48/000000/search.png"/><p>Search</p>
+                        <img className='icon' src="https://img.icons8.com/ios/48/000000/search.png" alt='magnifying glass'/><p>Search</p>
                         </Link>
                         <Link className='nav' to='/home/recipes'>
-                        <img className='icon' src="https://img.icons8.com/windows/32/000000/grip-lines-vertical.png"/><p>Recipe Box</p></Link>
+                        <img className='icon' src="https://img.icons8.com/windows/32/000000/grip-lines-vertical.png" alt='lines'/><p>Recipe Box</p></Link>
                         <br/>
-                        <Link to='/home/recipes/new'>Create Recipe</Link>
-                        <Link to='/recipes'>Liked Recipes</Link>
+                        <Link to='/home/recipes/new' className='nav'>
+                            <img className='icon' src="https://img.icons8.com/ios/50/000000/plus--v1.png" alt='plus sign' /><p>Create Recipe</p></Link>
+                        <Link to='/recipes' className='nav'>
+                        <img className='icon' src="https://img.icons8.com/material-outlined/24/000000/like--v1.png" alt='heart' /><p>Liked Recipes</p></Link>
                         <br/>
                         <hr className='solid'/>
                         <br/>
                         <div className='overflow'>
-                            <Link to='/home'>Recipe 1</Link>
-                            <Link to='/home'>Recipe 2</Link>
-                            <Link to='/home'>Recipe 3</Link>
-                            <Link to='/home'>Recipe 4</Link>
-                            <Link to='/home'>Recipe 5</Link>
-                            <Link to='/home'>Recipe 6</Link>
-                            <Link to='/home'>Recipe 1</Link>
-                            <Link to='/home'>Recipe 2</Link>
-                            <Link to='/home'>Recipe 3</Link>
-                            <Link to='/home'>Recipe 4</Link>
-                            <Link to='/home'>Recipe 5</Link>
-                            <Link to='/home'>Recipe 6</Link>
-                            <Link to='/home'>Recipe 1</Link>
-                            <Link to='/home'>Recipe 2</Link>
-                            <Link to='/home'>Recipe 3</Link>
-                            <Link to='/home'>Recipe 4</Link>
-                            <Link to='/home'>Recipe 5</Link>
-                            <Link to='/home'>Recipe 6</Link>
+                            <Link to='/home'>Meal Plan 1</Link>
+                            <Link to='/home'>Day before Marathon</Link>
+                            <Link to='/home'>Day after Marathon</Link>
+                            <Link to='/home'>Mentally on a Beach</Link>
+                            <Link to='/home'>Holiday Cheer</Link>
+                            <Link to='/home'>Typical Work Day</Link>
+                            <Link to='/home'>All My Faves</Link>
+                            <Link to='/home'>Spanish</Link>
+                            <Link to='/home'>Nana's Recipes</Link>
+                            <Link to='/home'>Mama Bear's favorites</Link>
+                            <Link to='/home'>Healthy Comfort</Link>
+                            <Link to='/home'>Dinner Party</Link>
+                            <Link to='/home'>Hungover AF</Link>
+                            <Link to='/home'>Italian Dinner Party</Link>
+                            <Link to='/home'>Parisian Dreams</Link>
+                            <Link to='/home'>Spice things up</Link>
+                            <Link to='/home'>We have guests</Link>
+                            <Link to='/home'>90s Nostalgia</Link>
                         </div>
                     </nav>
                 }
