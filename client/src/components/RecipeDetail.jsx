f361bdb979e6173eb137c7716228f1cf79056190
gooliajulia929 <juliadwyer93@gmail.com>
2021-12-14 18:35:23 -0500

    button and recipe detail css

--- client/src/components/RecipeDetail.jsx
+++ client/src/components/RecipeDetail.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { getOneRecipe } from '../services/recipe';
+import './RecipeDetail.css'
 
 export default function RecipeDetail({ handleRecipeDelete, currentUser }) {
     const [recipe, setRecipe] = useState(null)
@@ -15,21 +16,21 @@ export default function RecipeDetail({ handleRecipeDelete, currentUser }) {
     }, [id])
 
     return (
-        <div>
-            <h3>Recipe Detail</h3>
-            <h4>{recipe?.name}</h4>
+        <div className='detail'>
             <img src={recipe?.image_url} alt={recipe?.name}/>
-            <p>{recipe?.ingredients}</p>
-            { recipe?.user_id === currentUser?.id ?
-                <>
-                    <Link to={`/home/recipes/${recipe?.id}/edit`} >
-                        <button>Edit Recipe</button>
-                    </Link>
-                    <button onClick={() => handleRecipeDelete(recipe.id)}>Delete</button>
-                </>
-            : null
-            }
-
+            <div className='sub-detail'>
+                <h4 className='detail'>{recipe?.name}</h4>
+                <h5 className='detail'>Ingredients: {recipe?.ingredients}</h5>
+                { recipe?.user_id === currentUser?.id ?
+                    <>
+                        <Link to={`/home/recipes/${recipe?.id}/edit`} >
+                            <button className='detail'>Edit Recipe</button>
+                        </Link>
+                        <button className='detail'onClick={() => handleRecipeDelete(recipe.id)}>Delete</button>
+                    </>
+                : null
+                }
+            </div>
         </div>
     )
 }
