6e79c29f1b32990084294f899f5a2f6c56133fed
gooliajulia929 <juliadwyer93@gmail.com>
2021-12-14 19:02:01 -0500

    upgrade search screen css

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import './Search.css';
 
 export default function Search({recipes}) {
     const [searchResults, setSearchResults] = useState(recipes)
@@ -17,9 +18,12 @@ export default function Search({recipes}) {
         console.log(searchResults);
     },[searchKey])
     return (
-        <div>
-            <p>Search Page</p>
-            <input type='search' name='searchKey' value={searchKey} onChange={(e) => handleSearch(e)}/>
+        <div className='search'>
+            <div className='search-bar'>
+                <img className='icon' src="https://img.icons8.com/ios/48/000000/search.png" alt='magnifying glass'/>
+                <input type='search' name='searchKey' value={searchKey} onChange={(e) => handleSearch(e)} placeholder='Recipe names'/>
+            </div>
+
             {searchResults?.map(result => (
                 <div>
                     <h3>{result.name}</h3>
