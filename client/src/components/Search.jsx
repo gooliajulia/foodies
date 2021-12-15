6692a28a126fb5400b3fee25a2ff2e19d694b7e9
gooliajulia929 <juliadwyer93@gmail.com>
2021-12-15 18:36:55 -0500

    search recipe cards

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -23,13 +23,14 @@ export default function Search({recipes}) {
                 <img className='icon' src="https://img.icons8.com/ios/48/000000/search.png" alt='magnifying glass'/>
                 <input type='search' name='searchKey' value={searchKey} onChange={(e) => handleSearch(e)} placeholder='Recipe names'/>
             </div>
-
+            <div className='search-card-container'>
             {searchResults?.map(result => (
-                <div>
+                <div className='search-card'>
                     <h3>{result.name}</h3>
+                    <img src={result.image_url} alt={result.name} />
                 </div>
             ))}
-
+            </div>
         </div>
     )
 }
