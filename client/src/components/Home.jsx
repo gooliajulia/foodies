6cfa4e56bd04db25a73152e1b7c8635832ea2100
gooliajulia929 <juliadwyer93@gmail.com>
2021-12-14 19:25:36 -0500

    update seed data and ensure ingredient spotlight will always return a result

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,15 @@
 import './Home.css';
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 export default function Home({recipes, currentUser}) {
+    const [ingredientHighlight, setIngredientHighlight] = useState('oil');
+
+    useEffect(()=> {
+        if (recipes.length) {
+            setIngredientHighlight(recipes[Math.floor(Math.random()*3)].ingredients.split(', ')[Math.floor(Math.random()*3)]);
+        }
+    }, [recipes])
     return (
         <>
         <h4 className='home-highlight'>Discover new recipes</h4>
@@ -17,10 +25,10 @@ export default function Home({recipes, currentUser}) {
                         </Link>
                     )}
             </div>
-        <h4 className='home-highlight'>Ingredient Spotlight: Oil</h4>
+        <h4 className='home-highlight'>Ingredient Spotlight: {ingredientHighlight}</h4>
         <div className='discover-recipes'>
             {recipes?.filter(recipe => 
-            recipe.ingredients.toLowerCase().includes('oil')).map(recipe =>
+            recipe.ingredients.toLowerCase().includes(`${ingredientHighlight.toLowerCase()}`)).map(recipe =>
                 <Link to={`/home/recipes/${recipe.id}`}>
                     <div className='recipe-card'>
                         <h3>{recipe.name}</h3>
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,16 +11,24 @@ User.destroy_all
 
 @admin = User.create!(username: 'Julia', password: '123456', image_url: 'https://media-exp1.licdn.com/dms/image/D4D03AQGCF9ln-ItOJg/profile-displayphoto-shrink_400_400/0/1637721714007?e=1645056000&v=beta&t=i3JzwEg3UcWhA7d8sTImJF1Zyr33fpEmzjVsZpU9s3U')
 @gordon = User.create!(username: 'Gordon Ramsay', password: 'gordon', image_url: 'https://yt3.ggpht.com/bFpwiiOB_NLCVsIcVQ9UcwBjb1RzipnMmtNfLSWpeIaHboyGkBCq4KBitmovRbStk9WvIWIZOyo=s900-c-k-c0x00ffffff-no-rj')
+@ina = User.create!(username: 'Ina Garten', password: 'garten', image_url: 'https://www.chatelaine.com/wp-content/uploads/2018/01/ina2.jpg')
 
 puts "#{User.count} user accounts created!"
 
-10.times do
+3.times do
     Recipe.create!(name: Faker::Food.dish, ingredients:  "#{Faker::Food.ingredient}, #{Faker::Food.ingredient},  #{Faker::Food.ingredient}" , user:@admin, image_url: 'https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=749&q=80')
 end
 
-10.times do
+3.times do
     Recipe.create!(name: Faker::Food.dish, ingredients:  "#{Faker::Food.ingredient}, #{Faker::Food.ingredient},  #{Faker::Food.ingredient}" ,
     user:@gordon, image_url: 'https://images.unsplash.com/photo-1529042410759-befb1204b468?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80')
 end
+
+3.times do
+    Recipe.create!(name: Faker::Food.dish, ingredients:  "#{Faker::Food.ingredient}, #{Faker::Food.ingredient},  #{Faker::Food.ingredient}" ,
+    user:@ina, image_url: 'https://images.unsplash.com/photo-1546793665-c74683f339c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')
+end
+
+
 
 puts "#{Recipe.count} recipes created!"
